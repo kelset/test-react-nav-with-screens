9308b9de34ff080179fd97c58187f70ce1eee527
Lorenzo Sciandra <lorenzo.sciandra@gmail.com>
2018-09-10 17:13:51 +0100

    adding the react-native-screens lib

--- android/app/src/main/java/com/testnavigation/MainApplication.java
+++ android/app/src/main/java/com/testnavigation/MainApplication.java
@@ -3,6 +3,7 @@ package com.testnavigation;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.rnscreens.RNScreenPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +23,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNScreenPackage()
       );
     }
 
