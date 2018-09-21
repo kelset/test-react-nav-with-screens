3e4b4a3d1142809189a79bdad3694e82fdafab97
Lorenzo Sciandra <lorenzo.sciandra@gmail.com>
2018-09-21 12:27:36 +0100

    update to newer versions of packs & reduce clutter 🛀

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 import { RootNavigator } from './navigators/RootNavigator'
+import { useScreens } from 'react-native-screens'
+
+useScreens()
 
 export default class App extends React.Component {
   render() {
